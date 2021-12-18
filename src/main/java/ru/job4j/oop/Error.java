b413f4981e367504970e96f5967b68540d87e041
ASVidin <Andrey.Vidin@gmail.com>
2021-12-18 15:30:32 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error() {
+    }
+
+    public void printInfo() {
+        System.out.println("Active error: " + active);
+        System.out.println("Error's status: " + status);
+        System.out.println("Description: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error error404 = new Error(true, 404, "Not Found");
+        Error error502 = new Error(false, 502, "Bad Gateway");
+        Error error408 = new Error(true, 408, "Request Timeout");
+    }
+}
