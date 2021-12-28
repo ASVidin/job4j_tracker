25de3616beef2db2ce93017f247a1cf2eb06c568
ASVidin <Andrey.Vidin@gmail.com>
2021-12-28 12:55:56 +0300

    fix 3. Массивы и модели. [#174892]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -12,19 +12,16 @@ public class Library {
         books[2] = book3;
         books[3] = book4;
         System.out.println("Print all books:");
-        print(books, false);
+        for (Book book : books) {
+            System.out.println(book);
+        }
         books[0] = book4;
         books[3] = book1;
         System.out.println("\nPrint book called \"Clean code\":");
-        print(books, true);
-    }
-
-    public static void print(Book[] books, boolean checkName) {
-        for (int i = 0; i < books.length; i++) {
-            if (checkName && !books[i].getName().equals("Clean code")) {
-                continue;
+        for (Book book : books) {
+            if ("Clean code".equals(book.getName())) {
+                System.out.println(book);
             }
-            System.out.println("Book index: #" + i + " - " + books[i]);
         }
     }
 }
