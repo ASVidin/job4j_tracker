259da5812e6c776c0df294057b2991c3c8973ccd
ASVidin <Andrey.Vidin@gmail.com>
2022-01-08 14:59:52 +0300

    fix 1. Singleton [#94606]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,17 +1,18 @@
 package ru.job4j.tracker;
 
 public final class SingleTracker {
-    private static Tracker tracker = null;
+    private static SingleTracker singleTracker = null;
+    private Tracker tracker = new Tracker();
 
     private SingleTracker() {
 
     }
 
-    public static Tracker getInstance() {
-        if (tracker == null) {
-            tracker = new Tracker();
+    public static SingleTracker getInstance() {
+        if (singleTracker == null) {
+            singleTracker = new SingleTracker();
         }
-        return tracker;
+        return singleTracker;
     }
 
     public Item add(Item item) {
