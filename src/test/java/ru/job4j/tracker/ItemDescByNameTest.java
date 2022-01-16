5870b958d00fe2f510c9a4d5c08d34a484da69f5
ASVidin <Andrey.Vidin@gmail.com>
2022-01-16 22:03:36 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class ItemDescByNameTest {
+
+    @Test
+    public void whenDescCompare() {
+        Item item1 = new Item("One");
+        Item item2 = new Item("Two");
+        Item item3 = new Item("Three");
+        List<Item> items = new ArrayList<>();
+        items.add(item1);
+        items.add(item2);
+        items.add(item3);
+        Collections.sort(items, new ItemDescByName());
+        List<Item> expect = new ArrayList<>();
+        expect.add(item2);
+        expect.add(item3);
+        expect.add(item1);
+        assertThat(items, is(expect));
+    }
+}
