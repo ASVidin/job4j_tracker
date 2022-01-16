431d5f1bdedc0ea7054661152f4831dc0c380f39
ASVidin <Andrey.Vidin@gmail.com>
2022-01-16 11:08:27 +0300

    fix 1. Паспорт и жители [#212656 #251305]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -3,7 +3,8 @@ package ru.job4j.collection;
 import org.hamcrest.core.Is;
 import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.MatcherAssert.*;
+import static org.junit.Assert.assertFalse;
 
 public class PassportOfficeTest {
     @Test
@@ -12,5 +13,14 @@ public class PassportOfficeTest {
         PassportOffice office = new PassportOffice();
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), Is.is(citizen));
+    }
+
+    @Test
+    public void addDuplicatedCitizen() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Ivan Ivanov");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen1);
+        assertFalse(office.add(citizen2));
     }
 }
