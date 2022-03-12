0b4126923a70bfbe3fe12b17246f85a166c9fdff
ASVidin <Andrey.Vidin@gmail.com>
2022-03-12 14:15:31 +0300

    fix 2. Local-Variable Type Inference [#70626 #269637]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -11,21 +11,21 @@ import static org.hamcrest.Matchers.is;
 public class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("534");
+        var persons = phones.find("534");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
 
     @Test
     public void whenNotMatch() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("535");
+        var persons = phones.find("535");
         assertThat(persons, is(List.of()));
     }
 }
